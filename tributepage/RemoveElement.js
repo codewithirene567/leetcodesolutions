7b1298b90a5475b88c4c436fda6f38483e54a06c
codewithirene567 <irenejinscott@gmail.com>
2021-01-10 23:43:23 -0500

    added an else statement

--- tributepage/RemoveElement.js
+++ tributepage/RemoveElement.js
@@ -1,3 +1,9 @@
 function removeElement(nums, value) {
     let length = nums.length - 1
    while(length >= 0){
+    if(nums[0] === val){
+        nums.shift()
+    } else {
+        let temporary = nums.shift()
+        nums.push(temporary)
+ }
