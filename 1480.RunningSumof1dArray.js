d82316dad6bc9bd3a4d23d6f2f450a71ef72e684
codewithirene567 <irenejinscott@gmail.com>
2021-02-26 21:34:53 -0500

    found an alternate way to solve a previous solution

--- 1480.RunningSumof1dArray.js
+++ 1480.RunningSumof1dArray.js
@@ -8,3 +8,22 @@ var runningSum = function(nums) {
 //of the array like this: Input: nums = [1,2,3,4]
 //Output: [1,3,6,10]
 //Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+//Alternate way I found to solve solved below:
+var runningSum = function(nums) {
+    let answer=[];
+    let start=0;
+   for(let i=0;i<nums.length;i++){
+    start=start+nums[i];//start is assigned to adding the value of whatever number 
+    //we are on plus the starting value originally or previously each time
+    //the loop runs
+      arr.push(start);
+      //every time start is increasing we push that into an array
+       console.log(answer)
+       //the console.log looks like this
+    // [ 1 ]
+    // [ 1, 2 ]
+    // [1, 2, 3 ]
+   }
+   return answer;   
+};
